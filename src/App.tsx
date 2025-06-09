b4dc40e3372773baf8bcd3cd9d18e51a54b92931
vane <13525442@qq.com>
2025-06-09 11:56:12 +0800

    feat: 添加 ZustandFileUpload 组件至 App，增强文件上传功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import ZustandFileDownload from "./components/ZustandFileDownload";
+import ZustandFileUpload from "./components/ZustandFileUpload";
 
 const App = () => {
   return (
     <div style={{ padding: 16 }}>
+      <ZustandFileUpload />
       <ZustandFileDownload />
     </div>
   );
