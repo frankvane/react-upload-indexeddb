a43cf8e5ec938cd8061e4b1b85a7523601ea4c6f
vane <13525442@qq.com>
2025-07-22 18:01:46 +0800

    feat: 删除FileUpload组件

--- src/components/FileUpload/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-// 导出所有钩子函数
-export { useBatchUploader } from "./useBatchUploader";
-export { useFileOperations } from "./useFileOperations";
-export { useFileProcessor } from "./useFileProcessor";
-export { useIndexedDBFiles } from "./useIndexedDBFiles";
-export { useLocalStorageSettings } from "./useLocalStorageSettings";
-export { useNetworkStatusHandler } from "./useNetworkStatusHandler";
-export { useNetworkType } from "./useNetworkType";
-export { useUploadController } from "./useUploadController";
